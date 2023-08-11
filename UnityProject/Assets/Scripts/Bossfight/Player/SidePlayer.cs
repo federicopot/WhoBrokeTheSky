64f0ff7b5fe7beb1c1c22f2140c00b6a0501b4ce
cristian-zeni-marconi <cristian.zeni@marconirovereto.it>
2023-08-11 16:01:27 +0200

    added movement and jump

--- UnityProject/Assets/Scripts/Bossfight/Player/SidePlayer.cs
+++ UnityProject/Assets/Scripts/Bossfight/Player/SidePlayer.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using GBGUnity.Utility;
 
 public class SidePlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        MovementObject.MoveSide(this.GetComponent<Rigidbody2D>(), 5);
     }
+
+    void FixedUpdate(){
+        if(Input.GetKeyUp(KeyCode.Space)){
+            MovementObject.Jump(this.GetComponent<Rigidbody2D>(), 10);
+        }
+    }
+
 }
