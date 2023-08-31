4ed9f6d256baeca374b62b0a85c7ca3e39dfbb7a
cristian-zeni-marconi <cristian.zeni@marconirovereto.it>
2023-08-31 11:09:30 +0200

    fixing player movement

--- UnityProject/Assets/Scripts/Bossfight/Player/SidePlayer.cs
+++ UnityProject/Assets/Scripts/Bossfight/Player/SidePlayer.cs
@@ -5,21 +5,27 @@ using GBGUnity.Utility;
 
 public class SidePlayer : MonoBehaviour
 {
+
+    public float speed;
     
     void Start()
     {
         
     }
-
     
     void Update()
     {
-        MovementObject.MoveSide(this.GetComponent<Rigidbody2D>(), .5f, this.transform);
-        //transform.Translate(Input.GetAxis("Horizontal")*Time.deltaTime*3, 0, 0);
-
+        //MovementObject.MoveSide(this.GetComponent<Rigidbody2D>(), .5f, this.transform);
+        //transform.Translate(Input.GetAxis("Horizontal")*Time.deltaTime*speed, 0, 0);
         if(Input.GetKeyUp(KeyCode.Space)){
             MovementObject.Jump(this.GetComponent<Rigidbody2D>(), 7);
         }
+    }
+
+    void FixedUpdate(){
+
+        transform.position += new Vector3(Input.GetAxis("Horizontal")*Time.deltaTime*speed, 0, 0);
+
     }
 
 }
